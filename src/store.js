736b2af6547276a756bb4d867d73c55454b05be1
boertel <ben@comediadesign.com>
2019-05-02 18:19:31 -0700

    push on now

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ const store = createStore(
   reducers,
   compose(
     applyMiddleware(thunk, reduxPackMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
   )
 );
 export default store;
